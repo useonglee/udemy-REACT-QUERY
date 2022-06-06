7c7c7144a1a0ca2bf00ff7a7bfa8e4abefa58a40
useonglee <useong0830@naver.com>
2022-06-06 23:18:41 +0900

    50. 전역 리페치(re-fetch) 옵션

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -21,6 +21,11 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       onError: queryErrorHandler,
+      staleTime: 600000, // 10 minutes
+      cacheTime: 900000, // 15 minutes
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
     },
     mutations: {
       onError: queryErrorHandler,
