be9b47f25797d6d5a61b95b70257d75729ce0a14
useonglee <useong0830@naver.com>
2022-06-25 23:57:25 +0900

    82. react-query 커스텀 훅 테스트

--- base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -1,8 +1,36 @@
-// import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 
-// import { createWrapper } from '../../../test-utils';
+import { createQueryClientWrapper } from '../../../test-utils';
 import { useAppointments } from '../hooks/useAppointments';
+import { AppointmentDateMap } from '../types';
+
+const getAppointmentCount = (appointments: AppointmentDateMap) =>
+  Object.values(appointments).reduce(
+    (runningCount, appointmentsOnDate) =>
+      runningCount + appointmentsOnDate.length,
+    0,
+  );
 
 test('filter appointments by availability', async () => {
-  // test goes here
+  const { result, waitFor } = renderHook(() => useAppointments(), {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for the appointments to populate
+  await waitFor(() => getAppointmentCount(result.current.appointments) > 0);
+
+  const filteredAppointmentsLength = getAppointmentCount(
+    result.current.appointments,
+  );
+
+  // set to show all appointments
+  act(() => result.current.setShowAll(true));
+
+  // wait for the appointments to show more then when filtered
+  await waitFor(() => {
+    return (
+      getAppointmentCount(result.current.appointments) >
+      filteredAppointmentsLength
+    );
+  });
 });
